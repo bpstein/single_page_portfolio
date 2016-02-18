718d61b09e184e81468cce4d905f8a9485c1bf00
Ben Stein <bumstain@gmail.com>
2016-02-18 14:45:25 +0000

    Added opaque overlay for images

--- js/app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('#streetfighter').mouseenter(function() {
-    $(this).addClass('overlay');
-  })
-  .mouseleave(function() {
-    $(this).removeClass('overlay');
-  });
-});
